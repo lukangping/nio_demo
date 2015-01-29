2d2c3d55bef75d4b27c43721fbadf6b5a9fc4097
Kangping Lu <kangping.lu@rea-group.com>
2015-01-30 07:43:56 +0800

    KP: nio multiplexer done.

---
+++ src/main/java/com/nio/demo/nio/MultiplexerTimeServer.java
@@ -0,0 +1,49 @@
+package com.nio.demo.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * Created by kplu on 1/28/15.
+ */
+public class MultiplexerTimeServer implements Runnable{
+
+    private Selector selector;
+
+    public MultiplexerTimeServer() {
+        try {
+            selector = Selector.open();
+            ServerSocketChannel serverChannel = ServerSocketChannel.open();
+            serverChannel.configureBlocking(false);
+            serverChannel.socket().bind(new InetSocketAddress(8082));
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Time server start.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            selector.select();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void handleInput(SelectionKey key) {
+
+        if(key.isValid()){
+            if(key.isAcceptable()){
+                key.channel()
+            }
+        }
+
+    }
+}
